a701afed9f36a211e5b8a413635d160b691d992a
arfryer <arfryer@gmail.com>
2019-11-20 08:36:04 -0700

    Added PrintOutput

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,5 +1,22 @@
+# https://github.com/legoarf/Week12-csci102
 # Author: Alexander Fryer
 # Date: 11/20/19
 # Week12-utility.py
 # Course: CSCI102 - Section B
 
+################################################
+########   Function 1 : PrintOutput    #########
+################################################
+
+
+def PrintOutput(input_str):
+    print("OUTPUT {}".format(input_str))
+
+
+################################################
+############     Function Tests    #############
+################################################
+
+
+print("Testing PrintOutput ...")
+PrintOutput("This is a test string")
